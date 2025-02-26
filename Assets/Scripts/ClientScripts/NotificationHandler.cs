1ce275154d98a84bfebd298097735f0ac8160356
EnjoyWorld <denisavatar@hotmail.com>
2025-02-26 11:34:05 +0100

    Small notification upd
    - Added notification to browser and emails

--- Assets/Scripts/ClientScripts/NotificationHandler.cs
+++ Assets/Scripts/ClientScripts/NotificationHandler.cs
@@ -1,20 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class NotificationHandler : MonoBehaviour
 {
-    public enum NotificationType { General, Email }
+    [Serializable] public enum NotificationType { General = 1, Email = 2, Browser = 3 }
 
     [SerializeField] private GameObject _generalNotifcation;
     [SerializeField] private GameObject _emailNotification;
+    [SerializeField] private GameObject _browserNotification;
     private Dictionary<NotificationType, GameObject> _notificationDict;
     private void Start()
     {
         _notificationDict = new Dictionary<NotificationType, GameObject>()
         {
             {NotificationType.General, _generalNotifcation},
-            {NotificationType.Email, _emailNotification}
+            {NotificationType.Email, _emailNotification},
+            {NotificationType.Browser, _browserNotification}
         };
     }
 
@@ -33,5 +36,13 @@ public class NotificationHandler : MonoBehaviour
     public void HideNotification(NotificationType pNotification)
     {
         _notificationDict[pNotification].SetActive(false);
+    }
+    public void CallNotification(int pNotification)
+    {
+        _notificationDict[(NotificationType)pNotification].SetActive(true);
+    }
+    public void HideNotification(int pNotification)
+    {
+        _notificationDict[(NotificationType)pNotification].SetActive(false);
     }
 }
